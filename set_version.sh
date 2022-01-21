a32afeb58b64d040bc907597940800e79fdcee33
vesko <ggrrrr@gmail.com>
2022-01-21 22:14:20 +0200

    app version

--- set_version.sh
+++ set_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APP_HASH=`git log -n 1 --pretty=format:"%h"`
-APP_DATE=$(date '+%Y-%m-%d %H:%M:%S')
-
+APP_DATE=$(date '+%d-%m-%YT%H:%M')
+echo "${APP_HASH} ${APP_DATE} "
 sed -i "s/_hash_/${APP_HASH}/g" vue.config.js
 sed -i "s/_date_/${APP_DATE}/g" vue.config.js
