907c540e429d606bc2594be26678b18cf9c07c6a
vesko <ggrrrr@gmail.com>
2022-01-21 21:42:10 +0200

    version test

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,4 @@ module.exports = {
   transpileDependencies: ["vuetify"],
 };
 // const { gitDescribe, gitDescribeSync } = require("git-describe");
-process.env.VUE_APP_GIT_HASH = process.env.SHELL;
+process.env.VUE_APP_GIT_HASH = "__app__ver__";
