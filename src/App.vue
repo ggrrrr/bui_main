b9d3de362a1258ae7ab6273f5b1571c0b9c9fd71
vesko <ggrrrr@gmail.com>
2022-01-21 22:42:00 +0200

    asd

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,33 @@
       <!-- <v-card small class="flex" tile width="100%"> -->
       <!-- <v-card-text small tile class="subheading" width="100%"> -->
       <!-- <v-icon color="green" small>mdi-vuejs</v-icon> -->
-      <!-- <v-row> -->
-      <v-col class="subheading text-left"
-        >{{ build_hash }}.{{ build_date }}</v-col
-      >
-      <!-- <v-col></v-col> -->
-      <v-col class="subheading text-right">
-        <v-btn
-          text
-          small
-          href="https://vuetifyjs.com"
-          class="text-center"
-          target="_blank"
-          >vuetifyjs</v-btn
+      <v-row>
+        <v-col class="subheading text-left"
+          >{{ build_hash }}.{{ build_date }}</v-col
         >
-        <v-icon color="blue" small>mdi-ampersand</v-icon>
-        <!-- <v-icon color="green" small>mdi-vuejs</v-icon> -->
-        <!-- <v-icon color="blue" small>mdi-vuetify</v-icon> -->
-        <v-btn
-          text
-          small
-          href="https://vuejs.com"
-          class="text-center"
-          target="_blank"
-          >vuejs</v-btn
-        >
-      </v-col>
+        <!-- <v-col></v-col> -->
+        <v-col class="subheading text-right">
+          <v-btn
+            text
+            small
+            href="https://vuetifyjs.com"
+            class="text-center"
+            target="_blank"
+            >vuetifyjs</v-btn
+          >
+          <v-icon color="blue" small>mdi-ampersand</v-icon>
+          <!-- <v-icon color="green" small>mdi-vuejs</v-icon> -->
+          <!-- <v-icon color="blue" small>mdi-vuetify</v-icon> -->
+          <v-btn
+            text
+            small
+            href="https://vuejs.com"
+            class="text-center"
+            target="_blank"
+            >vuejs</v-btn
+          >
+        </v-col>
+      </v-row>
       <!-- </v-row> -->
       <!-- </v-card-text> -->
       <!-- </v-card> -->
